4aea368c71baf9bed069e69ad3d4c93bec6b6afd
Jasper O'Leary <jhaazpr@gmail.com>
2020-05-19 22:54:38 -0700

    untested bresenham

--- 2d/canvas.py
+++ 2d/canvas.py
@@ -1,13 +1,13 @@
 import numpy as np
-import cv2
+import imageio
 
 class Canvas
     def __init__(self, height, width):
-        self.MAX_PX_VAL = 255
+        self.BLACK = 255
         self.height = height
         self.width = width
         self.primitives = []
-        self.canvas = np.ones((height, width)) * MAX_PX_VAL
+        self.canvas = np.ones((height, width)) * BLACK
 
     def add_line(x0, y0, x1, y1):
         return self._add_primitive_struct('line', [x0, y0, x1, y1])
@@ -17,6 +17,9 @@ class Canvas
         self.primitives.append(struct)
         return struct
 
+    def set_pixel(self, x, y):
+        self.canvas[x, y] = self.BLACK
+
     def render_canvas(self):
         # TODO: calls all the drawing functions to draw structs
         # in self.primitives to the canvas
@@ -24,5 +27,52 @@ class Canvas
 
     def render_line(x0, y0, x1, y1):
         # TODO: bresenham
-        pass
+        if abs(y1 - y0) < abs(x1 - x0):
+            if x0 > x1:
+                self._render_line_low(x1, y1, x0, y0)
+            else:
+                self._render_line_low(x0, y0, x1, y1)
+        else:
+            if y0 > y1:
+                self._render_line_high(x1, y1, x0, y0)
+            else:
+                self._render_line_high(x0, y0, x1, y1)
+
+    def _render_line_low(x0, y0, x1, y1):
+        dx = x1 - x0
+        dy = y1 - y0
+        yi = 1
+        if dy < 0:
+            yi = -1
+            dy = -dy
+        D = 2 * dy - dx
+        y = y0
+
+        for x in range(x0, x1):
+            self.set_pixel(x, y)
+            if D > 0:
+                y += yi
+                D -= 2 * dx
+            D += 2 * dy
+
+    def _render_line_high(x0, y0, x1, y1):
+        dx = x1 - x0
+        dy = y1 - y0
+        xi = 1
+        if dy < 0:
+            xi = -1
+            dx = -dx
+        D = 2 * dx - dy
+        x = x0
+
+        for y in range(y0, y1):
+            self.set_pixel(x, y)
+            if D > 0:
+                x += xi
+                D -= 2 * dy
+            D += 2 * dx
+
+    def save_canvas(self, filename='img'):
+        imageio.imwrite(filename, self.canvas, 'png')
+        return filename
 
