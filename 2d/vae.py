810e973c42d7d7c034f4ae7d391cc30f4f22f908
Jasper O'Leary <jhaazpr@gmail.com>
2020-06-01 19:34:17 -0700

    oopify vae

--- 2d/vae.py
+++ 2d/vae.py
@@ -62,6 +62,10 @@ class VAE(nn.Module):
         self.fc4 = nn.Linear(400, 1024)
         self.sigmoid = nn.Sigmoid()
 
+        # Dr Diederik Kingma: as if VAEs weren't enough, he also gave us Adam!
+        self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
+
+
     def encode(self, x: Variable) -> (Variable, Variable):
         """Input vector x -> fully connected 1 -> ReLU -> (fully connected
         21, fully connected 22)
@@ -143,133 +147,125 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-model = VAE()
-if CUDA:
-    model.cuda()
-
-def load_dataset(is_test=False):
-    if is_test:
-        data_path = './test'
-    else:
-        data_path = './data'
-    train_dataset = datasets.ImageFolder(
-            root=data_path,
-            transform=transforms.Compose([
-                transforms.Grayscale(),
-                transforms.ToTensor()
-            ])
-    )
-    train_loader = torch.utils.data.DataLoader(
-            train_dataset,
-            batch_size=BATCH_SIZE,
-            num_workers=0,
-            shuffle=True
-    )
-    return train_loader
-
-
-def loss_function(recon_x, x, mu, logvar) -> Variable:
-    # how well do input x and output recon_x agree?
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 1024))
-
-    # KLD is Kullback–Leibler divergence -- how much does one learned
-    # distribution deviate from another, in this specific case the
-    # learned distribution from the unit Gaussian
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    # note the negative D_{KL} in appendix B of the paper
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    # Normalise by same number of elements as in reconstruction
-    KLD /= BATCH_SIZE * 1024
-
-    # BCE tries to make our reconstruction as accurate as possible
-    # KLD tries to push the distributions as close as possible to unit Gaussian
-    return BCE + KLD
-
-# Dr Diederik Kingma: as if VAEs weren't enough, he also gave us Adam!
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
-def train(epoch):
-    # toggle model to train mode
-    model.train()
-    train_loss = 0
-    # in the case of MNIST, len(train_loader.dataset) is 60000
-    # each `data` is of BATCH_SIZE samples and has shape [128, 1, 28, 28]
-    train_loader = load_dataset()
-    for batch_idx, (data, _) in enumerate(train_loader):
-        data = Variable(data)
-        if CUDA:
-            data = data.cuda()
-        optimizer.zero_grad()
-
-        # push whole batch of data through VAE.forward() to get recon_loss
-        recon_batch, mu, logvar = model(data)
-        # calculate scalar loss
-        loss = loss_function(recon_batch, data, mu, logvar)
-        # calculate the gradient of the loss w.r.t. the graph leaves
-        # i.e. input variables -- by the power of pytorch!
-        loss.backward()
-        # train_loss += loss.data[0]
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % LOG_INTERVAL == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
-
-
-def test(epoch):
-    # toggle model to test / inference mode
-    model.eval()
-    test_loss = 0
-
-    # each data is of BATCH_SIZE (default 128) samples
-    test_loader = load_dataset(is_test=True)
-    for i, (data, _) in enumerate(test_loader):
-        if CUDA:
-            # make sure this lives on the GPU
-            data = data.cuda()
-
-        # we're only going to infer, so no autograd at all required: volatile=True
-        with torch.no_grad():
+    def train_model(self, epoch):
+        # toggle model to train mode
+        self.train()
+        train_loss = 0
+        # in the case of MNIST, len(train_loader.dataset) is 60000
+        # each `data` is of BATCH_SIZE samples and has shape [128, 1, 28, 28]
+        train_loader = self.load_dataset()
+        for batch_idx, (data, _) in enumerate(train_loader):
             data = Variable(data)
-        recon_batch, mu, logvar = model(data)
-        test_loss += loss_function(recon_batch, data, mu, logvar).data.item()
-        if i == 0:
-          n = min(data.size(0), 8)
-          # for the first 128 batch of the epoch, show the first 8 input digits
-          # with right below them the reconstructed output digits
-          comparison = torch.cat([data[:n],
-                                  recon_batch.view(BATCH_SIZE, 1, 32, 32)[:n]])
-          save_image(comparison.data.cpu(),
-                     'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+            if CUDA:
+                data = data.cuda()
+            self.optimizer.zero_grad()
 
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+            # push whole batch of data through VAE.forward() to get recon_loss
+            recon_batch, mu, logvar = self.forward(data)
+            # calculate scalar loss
+            loss = self.loss_function(recon_batch, data, mu, logvar)
+            # calculate the gradient of the loss w.r.t. the graph leaves
+            # i.e. input variables -- by the power of pytorch!
+            loss.backward()
+            # train_loss += loss.data[0]
+            train_loss += loss.item()
+            self.optimizer.step()
+            if batch_idx % LOG_INTERVAL == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader),
+                    loss.item() / len(data)))
+
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+              epoch, train_loss / len(train_loader.dataset)))
+
+    def test(self, epoch):
+        # toggle model to test / inference mode
+        self.eval()
+        test_loss = 0
+
+        # each data is of BATCH_SIZE (default 128) samples
+        test_loader = self.load_dataset(is_test=True)
+        for i, (data, _) in enumerate(test_loader):
+            if CUDA:
+                # make sure this lives on the GPU
+                data = data.cuda()
+
+            # we're only going to infer, so no autograd at all required: volatile=True
+            with torch.no_grad():
+                data = Variable(data)
+            recon_batch, mu, logvar = self.forward(data)
+            test_loss += self.loss_function(recon_batch, data, mu, logvar).data.item()
+            if i == 0:
+              n = min(data.size(0), 8)
+              # for the first 128 batch of the epoch, show the first 8 input digits
+              # with right below them the reconstructed output digits
+              comparison = torch.cat([data[:n],
+                                      recon_batch.view(BATCH_SIZE, 1, 32, 32)[:n]])
+              save_image(comparison.data.cpu(),
+                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+
+    def load_dataset(self, is_test=False):
+        if is_test:
+            data_path = './test'
+        else:
+            data_path = './data'
+        train_dataset = datasets.ImageFolder(
+                root=data_path,
+                transform=transforms.Compose([
+                    transforms.Grayscale(),
+                    transforms.ToTensor()
+                ])
+        )
+        train_loader = torch.utils.data.DataLoader(
+                train_dataset,
+                batch_size=BATCH_SIZE,
+                num_workers=0,
+                shuffle=True
+        )
+        return train_loader
+
+    def loss_function(self, recon_x, x, mu, logvar) -> Variable:
+        # how well do input x and output recon_x agree?
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 1024))
+
+        # KLD is Kullback–Leibler divergence -- how much does one learned
+        # distribution deviate from another, in this specific case the
+        # learned distribution from the unit Gaussian
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        # note the negative D_{KL} in appendix B of the paper
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # Normalise by same number of elements as in reconstruction
+        KLD /= BATCH_SIZE * 1024
+
+        # BCE tries to make our reconstruction as accurate as possible
+        # KLD tries to push the distributions as close as possible to unit Gaussian
+        return BCE + KLD
 
 
-for epoch in range(1, EPOCHS + 1):
-    train(epoch)
-    test(epoch)
+if __name__ == '__main__':
+    vae = VAE()
+    for epoch in range(1, EPOCHS + 1):
+        vae.train_model(epoch)
+        vae.test(epoch)
 
-    # 64 sets of random ZDIMS-float vectors, i.e. 64 locations / MNIST
-    # digits in latent space
-    sample = Variable(torch.randn(64, ZDIMS))
-    if CUDA:
-        sample = sample.cuda()
-    sample = model.decode(sample).cpu()
+        # 64 sets of random ZDIMS-float vectors, i.e. 64 locations / MNIST
+        # digits in latent space
+        sample = Variable(torch.randn(64, ZDIMS))
+        if CUDA:
+            sample = sample.cuda()
+        sample = vae.decode(sample).cpu()
 
-    # save out as an 8x8 matrix of MNIST digits
-    # this will give you a visual idea of how well latent space can generate things
-    # that look like digits
-    save_image(sample.data.view(64, 1, 32, 32),
-               'results/sample_' + str(epoch) + '.png')
+        # save out as an 8x8 matrix of MNIST digits
+        # this will give you a visual idea of how well latent space can generate things
+        # that look like digits
+        save_image(sample.data.view(64, 1, 32, 32),
+                   'results/sample_' + str(epoch) + '.png')
 
